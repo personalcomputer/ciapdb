168f8e7509a93867252f68056b98d9f880d34343
John <john@johngm.com>
2015-09-24 02:49:26 -0700

    added parsing of countries in topics

--- scripts/5-process-text-2.py
+++ scripts/5-process-text-2.py
@@ -12,28 +12,27 @@ import sqlite3
 
 from common import *
 
-matplotlib.style.use('ggplot')
 
 def main():
   db = sqlite3.connect(DB_NAME, detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
   db_c = db.cursor()
 
-  db_c.execute('CREATE TABLE IF NOT EXISTS countries (topic TEXT, doc_number INT, FOREIGN KEY(doc_number) REFERENCES briefs(doc_number))')
+  db_c.execute('CREATE TABLE IF NOT EXISTS countries (country TEXT, doc_number INT, FOREIGN KEY(doc_number) REFERENCES briefs(doc_number))')
   db_c.execute('DELETE FROM countries')
   db.commit()
 
   #all_countries = {} #'country':set([associated docs])
 
-  topics_raw = db_c.execute('WITH cte AS topic,(SELECT count(*) as c FROM topics_raw GROUP BY topic) SELECT topic,c FROM cte WHERE c>=2 ORDER BY c DESC').fetchall()
+  topics_raw = db_c.execute('WITH cte AS (SELECT topic, COUNT(*) as c FROM topics_raw GROUP BY topic) SELECT topic,c FROM cte WHERE c>=2 ORDER BY c DESC').fetchall()
   for result in topics_raw:
     topic = result[0]
     frequency = result[1]
 
     countries=[t for t in topic.split('-') if t != '']
-    associated_docs = db_c.execute('SELECT doc_number FROM raw_topics WHERE topic==?',topic).fetchall()
+    associated_docs = db_c.execute('SELECT doc_number FROM topics_raw WHERE topic==?',(topic,)).fetchall()
     for country in countries:
       for associated_doc in associated_docs:
-        db_c.execute('INSERT INTO countries (country,doc_number) VALUES (?,?)', country,associated_doc)
+        db_c.execute('INSERT INTO countries (country,doc_number) VALUES (?,?)', (country,associated_doc[0]))
 
 
   db.commit()
